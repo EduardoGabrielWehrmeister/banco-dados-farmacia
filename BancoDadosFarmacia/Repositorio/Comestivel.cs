e0beeb31ef8f13f37b5e54e7c182d98be5123a25
EduardoGabrielWehrmeister <dudu245@live.com>
2019-06-04 16:26:07 -0300

    Criado o Banco de dados Comestiveis.Criado também Model com suas devidas propriedades.E também o Repositório!

---
+++ BancoDadosFarmacia/Repositorio/Comestivel.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class ComestivelRepositorio
+    {
+
+        }
+    }
+}
---
+++ BancoDadosFarmacia/Repositorio/Estrutura.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE comestiveis(
+ id INT PRIMARY KEY IDENTITY(1,1),
+ nome VARCHAR(50),
+ valor DECIMAL(10,2),
+ data_vencimento DATETIME2,
+ quantidade INT,
+ marca VARCHAR(100),
+ );
